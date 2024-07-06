24e2f8f09506ae4f68c69f9c73c3a293b38be9d8
Nasif Raihan <nasif.raihan78@gmail.com>
2024-07-06 11:01:11 +0600

    initiated the project

---
+++ pattern-api/blog/models.py
@@ -0,0 +1,55 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email: str, username: str, password: str = None):
+        user = self.model(email=self.normalize_email(email), username=username)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email: str, username: str, password: str = None):
+        user = self.create_user(email, username, password)
+        user.is_admin = True
+        user.is_staff = True
+        user.is_active = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(verbose_name="email", max_length=255, unique=True)
+    username = models.CharField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
+
+    def __str__(self) -> str:
+        return f"{self.username}"
+
+    @staticmethod
+    def has_perm(perm, obj=None) -> bool:
+        return True
+
+    @staticmethod
+    def has_model_perms(app_level) -> bool:
+        return True
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.title}"
