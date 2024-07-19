8d8979f5409720e192089f68fb34905999d32fbb
nasif raihan <nasif.raihan78@gmail.com>
2024-07-20 01:11:31 +0600

    added review model

--- pattern-api/domain/model/__init__.py
+++ pattern-api/domain/model/__init__.py
@@ -1,5 +1,6 @@
 from .blog_post import BlogPost
 from .comment import Comment
 from .post_score import PostScore
+from .review import Review
 from .user import User
 from .user_score import UserScore
---
+++ pattern-api/domain/model/review.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+from .blog_post import BlogPost
+
+
+@dataclass
+class Review:
+    title: str
+    rating: float
+    content: str
+    pros: str
+    cons: str
+    idea: str
+    recommendation: str
+    author_feedback: str
+    post: BlogPost
