50b28da82557ba3be10528fbdce9455a9f852617
nasif raihan <nasif.raihan78@gmail.com>
2024-07-21 12:07:23 +0600

    added API urls

--- pattern-api/blog/urls.py
+++ pattern-api/blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
-from .views import BlogPostAPI, UserAPI
+from .views import BlogPostAPI, UserAPI, ReviewAPI, CommentAPI, ScoreAPI
 
 urlpatterns = [
     path("blog/", csrf_exempt(BlogPostAPI.as_view()), name="blog"),
     path("user/", csrf_exempt(UserAPI.as_view()), name="user"),
+    path("comment/", csrf_exempt(CommentAPI.as_view()), name="comment"),
+    path("review/", csrf_exempt(ReviewAPI.as_view()), name="review"),
+    path("score/", csrf_exempt(ScoreAPI.as_view()), name="score"),
 ]
