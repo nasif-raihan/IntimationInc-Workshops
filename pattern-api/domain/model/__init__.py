f44139e2e72195897692c7b10aa78cde5ff9539d
nasif raihan <nasif.raihan78@gmail.com>
2024-07-22 18:10:54 +0600

    added Video model

--- pattern-api/domain/model/__init__.py
+++ pattern-api/domain/model/__init__.py
@@ -4,3 +4,4 @@ from .post_score import PostScore
 from .review import Review
 from .user import User
 from .user_score import UserScore
+from .video import Video
---
+++ pattern-api/domain/model/video.py
@@ -0,0 +1,7 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Video:
+    video_id: str
+    url: str
