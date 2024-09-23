3d99a77ce5747e14ce23dfb3039601b9cd7019a7
nasif raihan <nasif.raihan78@gmail.com>
2024-09-23 21:34:07 +0600

    implemented nginx as load balancer

--- nginx/server_one/main.py
+++ nginx/server_one/main.py
@@ -2,8 +2,17 @@ from uuid import uuid4
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 @app.get("/message")
